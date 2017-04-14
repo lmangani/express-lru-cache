fd902240e23a5a6619f66c162469b2afa3f5bb85
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-04-14 15:05:40 +0200

    remove example client

--- client.js
+++
@@ -1,9 +0,0 @@
-var debug = require('debug')('express-gun:client')
-var Gun = require('gun')
-var gun = Gun(['http://localhost:3000/gun'])
-
-var data = gun.get('data')
-data.path('message').map().on(msg => {
-  var date = new Date()
-  debug(`${date.toString()} Message recieved ${JSON.stringify(msg.test, null, 3)}`)
-})
