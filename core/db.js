ce63e6353500de177adfbb5d92973d4751e6d9a8
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-04-14 15:17:24 +0200

    Update db.js

--- core/db.js
+++ core/db.js
@@ -1,9 +1,8 @@
 var LRU = require("lru-cache")
-  , options = { max: 5000
+  , options = { max: process.env.MAX || 3000
               , length: function (n, key) { return n * 2 + key.length }
 	      , dispose: function (key, n) { return key.length }
               , maxAge: 1000 * 60 * 60 }
   , cache = LRU(options)
-  , otherCache = LRU(50) // sets just the max size
 
 module.exports = cache
